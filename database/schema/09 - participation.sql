d3e62ec254ea0533166196cd07f3704360ed3fae
Sérgio Filho Garcia Paim <sergio.filho@outlook.com>
2024-06-05 11:49:45 -0300

    Pequenas correções nas tables.

--- database/schema/09 - participation.sql
+++ database/schema/09 - participation.sql
@@ -4,14 +4,14 @@ GO
 CREATE TABLE Participation (
 Selection int NOT NULL,
 Game int NOT NULL,
-Quarters int NOT NULL
+Quarter int NOT NULL
 );
 
 ALTER TABLE Participation
-ADD CONSTRAINT PK_Quarters PRIMARY KEY (Selection, Game, Quarters)
+ADD CONSTRAINT PK_Participation PRIMARY KEY (Selection, Game, Quarter)
 
 ALTER TABLE Participation
-ADD FOREIGN KEY (Game, Quarters) REFERENCES Quarters(Game, Quarters);
+ADD FOREIGN KEY (Game, Quarter) REFERENCES Quarters(Game, Quarter);
 
 ALTER TABLE Participation
 ADD FOREIGN KEY (Selection) REFERENCES Selection(Id);
--- database/schema/10 - plays.sql
+++ database/schema/10 - plays.sql
@@ -5,13 +5,13 @@ CREATE TABLE Plays (
 Id int IDENTITY(1,1) PRIMARY KEY,
 Selected int,
 Game int,
-Quarters int,
-StatusType int,
-Quantity int,
+Quarter int,
+PlayType int,
+Amount int,
 );
 
 ALTER TABLE Plays
-ADD CONSTRAINT UC_Plays UNIQUE (Selected, Game, Quarters);
+ADD CONSTRAINT UC_Plays UNIQUE (Selected, Game, Quarter);
 
 ALTER TABLE Plays
-ADD FOREIGN KEY (Selected, Game, Quarters) REFERENCES Participation(Selection, Game, Quarters);
+ADD FOREIGN KEY (Selected, Game, Quarter) REFERENCES Participation(Selection, Game, Quarter);
