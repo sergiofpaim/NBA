db6a9466651eb6116093f05bb3fd83a8689713f9
Sérgio Filho Garcia Paim <sergio.filho@outlook.com>
2024-06-23 10:48:26 -0300

    Constraint Player Positions added

--- database/schema/04 - player.sql
+++ database/schema/04 - player.sql
@@ -8,4 +8,6 @@ Id int PRIMARY KEY ,
 Name varchar(50),
 BornOn date,
 Position varchar(20)
+CONSTRAINT CK_Player_Position CHECK (Position IN ('Shooting Guard', 'Point Guard', 'Power Forward',
+                                                  'Small Forward', 'Center'))
 );
