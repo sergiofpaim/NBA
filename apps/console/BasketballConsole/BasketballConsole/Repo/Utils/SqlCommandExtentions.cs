c575613a2903dead3d8bbba0a916af0e25ded847
Sérgio Filho Garcia Paim <sergio.filho@outlook.com>
2024-07-30 15:24:32 -0300

    Adicionado RepoSQL

---
+++ apps/console/BasketballConsole/BasketballConsole/Repo/Utils/SqlCommandExtentions.cs
@@ -0,0 +1,45 @@
+﻿using System.Data.SqlClient;
+using System.Text;
+
+namespace NBA.Repo.Utils
+{
+    internal static class SqlCommandExtensions
+    {
+        public static string ShowCommand(this SqlCommand cmd)
+        {
+            StringBuilder commandText = new StringBuilder(cmd.CommandText);
+
+            foreach (SqlParameter param in cmd.Parameters)
+            {
+                string parameterValue;
+
+                // Handle NULL values
+                if (param.Value == DBNull.Value)
+                {
+                    parameterValue = "NULL";
+                }
+                else if (param.Value is string || param.Value is DateTime)
+                {
+                    // Handle string and DateTime values by enclosing them in quotes
+                    parameterValue = $"'{param.Value}'";
+                }
+                else
+                {
+                    // Handle other data types without quotes
+                    parameterValue = param.Value.ToString();
+                }
+
+                // Replace parameter name with parameter value
+                commandText.AppendFormat(" {0} = {1},", param.ParameterName, parameterValue);
+            }
+
+            if (cmd.Parameters.Count > 0)
+            {
+                // Remove the last comma
+                commandText.Length--;
+            }
+
+            return commandText.ToString();
+        }
+    }
+}
