a0a98ea1f2233af91e1e16353a958c2d16021f3a
Sérgio Filho Paim <sergio.filho@outlook.com>
2025-01-21 10:18:47 -0300

    Mudanças table

--- Next with CosmosDB/client/my-next-app/src/components/TableComponent.tsx
+++ Next with CosmosDB/client/my-next-app/src/components/TableComponent.tsx
@@ -31,77 +31,81 @@ const TableComponent: React.FC<TableProps<any>> = ({
     itemHeight,
     sx,
 }) => (
-    <TableContainer
-        component={Paper}
-        sx={{
-            maxHeight: height || '600px',
-            overflow: 'auto',
-            borderRadius: 0,
-            borderColor: 'rgba(0, 0, 0, 0.1)',
-            backgroundColor: 'rgba(0, 0, 0, 0.1)',
-            ...sx,
-        }}
-    >
-        <Table stickyHeader>
-            <TableHead>
-                <TableRow>
-                    <TableCell
-                        align="center"
-                        colSpan={items.length > 0 ? renderItem(items[0]).length : 1}
-                        sx={{
-                            backgroundColor: globalTheme.palette.background.default,
-                            borderBottomColor: 'rgba(0, 0, 0, 0.1)',
-                            borderBottomWidth: "10px",
-                        }}
-                    >
-                        <Typography
+    <Paper sx={{
+        padding: 0,
+        borderRadius: 0,
+        borderColor: 'rgba(0, 0, 0, 0.1)',
+        backgroundColor: globalTheme.palette.background.default
+    }}>
+        <TableContainer
+            sx={{
+                maxHeight: height || '600px',
+                borderRadius: 0,
+                borderColor: 'rgba(0, 0, 0, 0.1)',
+                backgroundColor: 'rgba(0, 0, 0, 0.1)',
+                ...sx,
+            }}
+        >
+            <Table stickyHeader >
+                <TableHead>
+                    <TableRow>
+                        <TableCell
+                            align="center"
+                            colSpan={items.length > 0 ? renderItem(items[0]).length : 1}
                             sx={{
-                                fontSize: '1.5rem',
-                                justifyContent: 'center',
-                                textAlign: 'center',
-                                '@media (max-width: 400px)': {
-                                    fontSize: '1rem',
+                                backgroundColor: globalTheme.palette.background.default,
+                                borderBottomColor: 'rgba(0, 0, 0, 0.1)',
+                                borderBottomWidth: "10px",
+                            }}
+                        >
+                            <Typography
+                                sx={{
+                                    fontSize: '1.5rem',
+                                    justifyContent: 'center',
+                                    textAlign: 'center',
+                                    '@media (max-width: 400px)': {
+                                        fontSize: '1rem',
+                                    },
+                                }}
+                            >
+                                {label}
+                            </Typography>
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {items.map((item, index) => (
+                        <TableRow
+                            key={index}
+                            onClick={() => handleItemClick(item)}
+                            sx={{
+                                cursor: 'pointer',
+                                backgroundColor: globalTheme.palette.background.default,
+                                '&:hover': {
+                                    backgroundColor: 'rgba(0, 0, 0, 0.1)'
                                 },
                             }}
                         >
-                            {label}
-                        </Typography>
-                    </TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {items.map((item, index) => (
-                    <TableRow
-                        key={index}
-                        hover
-                        onClick={() => handleItemClick(item)}
-                        sx={{
-                            cursor: 'pointer',
-                            backgroundColor: globalTheme.palette.background.default,
-                            '&:hover': {
-                                backgroundColor: 'rgba(0, 0, 0, 0.1)'
-                            },
-                        }}
-                    >
-                        {renderItem(item).map((cellContent, cellIndex) => (
-                            <TableCell
-                                key={cellIndex}
-                                sx={{
-                                    borderBottomWidth: "0px",
-                                    height: itemHeight,
-                                    borderBottomColor: 'rgba(0, 0, 0, 0.1)',
-                                    alignItems: 'center',
-                                    padding: 0,
-                                }}
-                            >
-                                {cellContent}
-                            </TableCell>
-                        ))}
-                    </TableRow>
-                ))}
-            </TableBody>
-        </Table>
-    </TableContainer>
+                            {renderItem(item).map((cellContent, cellIndex) => (
+                                <TableCell
+                                    key={cellIndex}
+                                    sx={{
+                                        borderBottomWidth: "0px",
+                                        height: itemHeight,
+                                        borderBottomColor: 'rgba(0, 0, 0, 0.1)',
+                                        alignItems: 'center',
+                                        padding: 0,
+                                    }}
+                                >
+                                    {cellContent}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    </Paper>
 );
 
 export default TableComponent;
